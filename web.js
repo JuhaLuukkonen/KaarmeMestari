1aea9f7ea4b33ff16d1241b1ccedbf420daa7870
JuhaLuukkonen <32368550+JuhaLuukkonen@users.noreply.github.com>
2018-08-28 07:38:32 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,35 +1,35 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-// Mutable state
+// Muuttuva tila
 let state = initialState()
 
-// Position helpers
+// Sijainnin auttajat
 const x = c => Math.round(c * canvas.width / state.cols)
 const y = r => Math.round(r * canvas.height / state.rows)
 
-// Game loop draw
+// Peli luuppi piirrä
 const draw = () => {
-  // clear
+  // Tyhjennä
   ctx.fillStyle = '#232323'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-  // draw snake
+  // Piirrä käärme
   ctx.fillStyle = 'rgb(0,200,50)'
   state.snake.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
 
-  // draw apples
+  // Piirrä omena
   ctx.fillStyle = 'rgb(255,50,0)'
   ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1))
 
-  // add crash
+  // Lisää törmäys
   if (state.snake.length == 0) {
     ctx.fillStyle = 'rgb(255,0,0)'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
   }
 }
 
-// Game loop update
+// Peli luuppi päivitys
 const step = t1 => t2 => {
   if (t2 - t1 > 100) {
     state = next(state)
@@ -40,7 +40,7 @@ const step = t1 => t2 => {
   }
 }
 
-// Key events
+// Avain tapahtumat
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'w': case 'h': case 'ArrowUp':    state = enqueue(state, NORTH); break
@@ -50,5 +50,5 @@ window.addEventListener('keydown', e => {
   }
 })
 
-// Main
+// Pääosio
 draw(); window.requestAnimationFrame(step(0))
